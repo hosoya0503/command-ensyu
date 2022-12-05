4850435b5b60e8c9c9a0b4290a64fdf27333f506
hosoya <r.hosoya.sys22@morijyobi.ac.jp>
2022-12-05 16:02:21 +0900

    Main.java追記

--- Main.java
+++ Main.java
@@ -1,15 +1,31 @@
 import java.util.Scanner;
 
-public class Main{
-	public static void main(String[] args){
-		Scanner scanner = new Scanner(System.in);
-		System.out.print("体重(kg):");
-		double weight = scanner.nextDouble();
-		System.out.print("身長(cm):");
-		double height = scanner.nextDouble();
-		height = height/100;
-		height = height*height;
-		double bmi = weight/height;
-		System.out.println("BMI:"+bmi);
+public class Bmi {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("一人目の身長を入力してください。");
+		System.out.println("単位はcmではなくmです。");
+		System.out.print("一人目の身長：");
+		double height1 = sc.nextDouble();
+		System.out.println("一人目の体重を入力してください。");
+		System.out.print("一人目の体重：");
+		double weight1 = sc.nextDouble();
+		System.out.println("一人目のBMIを計算します。");
+		double bmi1 = weight1/(height1*height1);
+		System.out.println("一人目のBMI：" + String.format("%.2f",bmi1));
+
+		System.out.println("二人目の身長を入力してください。");
+                System.out.println("単位はcmではなくmです。");
+                System.out.print("二人目の身長：");
+                double height2 = sc.nextDouble();
+                System.out.println("二人目の体重を入力してください。");
+                System.out.print("二人目の体重：");
+                double weight2 = sc.nextDouble();
+                System.out.println("二人目のBMIを計算します。");
+                double bmi2 = weight2/(height2*height2);
+                System.out.println("二人目のBMI：" + String.format("%.2f",bmi2));
+
+		sc.close();
 	}
 }
